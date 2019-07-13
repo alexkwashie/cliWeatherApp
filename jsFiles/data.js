81aadee361a03001a8e63bfa0a4c7f749c1c1db8
Alex A. Kwashie <alex.kwashie@hotmail.com>
2019-07-13 23:39:30 +0100

    setting await function

--- jsFiles/data.js
+++ jsFiles/data.js
@@ -1,2 +1,22 @@
 
 const key = "MqMeA7ZYWrWyHy1XFAAmltSe586cC9E6";
+
+const getCity = async (city) =>{
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+
+    const query = `?apikey=${key}&q=${city}`;
+    // '?' means adding a list of other Api parameters, '&' means adding filter
+
+    const responds = await fetch(base + query); //So this fetches http://dataservice.accuweather.com/locations/v1/cities/search + ?apiKey=${key}&q=${city}`
+
+    //convert data to json
+    const data = await responds.json();
+
+return data[0];
+
+};
+
+
+getCity('london')
+.then(data => console.log(data))
+.catch(err => console.log('error: Error Occurred'));
