72fa5e9d140283aec4e91376cce664e8fe256498
alexkwashie <alex.kwashie@hotmail.com>
2019-07-14 03:21:26 +0100

    Updating UI etc

--- jsFiles/script.js
+++ jsFiles/script.js
@@ -1,7 +1,51 @@
  
  const input1 = document.querySelector('.change-location');
 
-input1.addEventListener('submit', e => {
+ const city_Name = document.querySelector('h5');
+
+ const updateUI = (data) =>{
+    const cityDetails1 = data.cityDetails;
+    const weather1 = data.weather;
+
+    //update html template
+    city_Name.innerHTML = `
+    <h5 class="my-3 cityName">${cityDetails1.EnglishName}</h5>
+            <h4 class="my-3">${weather1.WeatherText}</h4>
+            <div class="display-4 my-3">
+                <span>${weather1.Temperature.Metric.Value}</span>
+                <p>&deg;C</p>
+            </div>`;
+ };
+
+
+ const updateCity = async (city) =>{
+ 
+    //Use the await keyword because it return a promise and every promise might take time to oad data from api etc.
+    const cityDetials = await getCity(city);
+    const weather = await getWeather(cityDetials.Key);
+
+    //return multiples outputs as object
+    return {
+        cityDetails : cityDetials,
+        weather : weather
+    }
+
+ };
+
+
+
+
+ input1.addEventListener('submit', e => {
     e.preventDefault();
-    console.log(input1.value);
+
+    //get city value
+    let val = input1.city.value.trim();
+    input1.reset();
+
+    //update City name in UI
+    updateCity(val)
+    .then(data => updateUI(data))
+    .catch(err => updateUI((err)))
+
 });
+
